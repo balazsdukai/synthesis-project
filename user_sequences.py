f6041ccea0d07bc12013539ea17cef0ba8338cdf
balazsdukai <balazs.dukai@gmail.com>
2016-04-29 16:30:52 +0200

    sequenceUsers implemented TODO: filter records on stay duration

--- user_sequences.py
+++ user_sequences.py
@@ -10,21 +10,65 @@ except:
 # This routine creates a cursor which will be used throughout of your database programming with Python.
 cur = conn.cursor()
 
+# Globals
+ITERATOR = 0 # iterator used later in script
+MAPLOC = 4 # location of the 'maploc' field
+
+def getTime(item):
+    return item[2] # location of the timestamp in a record
+
+
+def getBuilding(string):
+    s = string.find(">") + 2
+    e = string.find(">", s) - 1
+    return string[s:e]
+
+# Get records by mac address
 cur.execute("SELECT distinct mac from wifilog")
 macs = cur.fetchall()
 print "macs fetched"
 
-iterator = 0
-
-cur.execute("select * from wifilog where mac='"+macs[iterator][0]+"';")
+cur.execute("select * from wifilog where mac='"+macs[ITERATOR][0]+"';")
 mac0 = cur.fetchall()
 
+# Get records by username
 cur.execute("SELECT distinct username from wifilog")
 users = cur.fetchall()
 print "users fetched"
 
-cur.execute("select * from wifilog where username='"+users[iterator][0]+"';")
+cur.execute("select * from wifilog where username='"+users[ITERATOR][0]+"';")
 user0 = cur.fetchall()
+
+user0_sorted = sorted(user0, key=getTime) # sort records by datetime
+user0_seq = []
+for i in user0_sorted:
+    seq = getBuilding(i[MAPLOC])
+    user0_seq.append(seq)
+
+def sequenceUsers(users, limit=50):
+    MAPLOC = 4 # location of the 'maploc' field
+    sequences = []
+    for i in users[ :limit]:
+        username = i[0]
+        cur.execute("select * from wifilog where username='" + username + "';")
+        user = cur.fetchall()
+        user_sorted = sorted(user, key=getTime)  # sort records by datetime
+        seq = []
+        for i in user0_sorted:
+            s = getBuilding(i[MAPLOC])
+            seq.append(s)
+        u_seq = (username, seq)
+        sequences.append(u_seq)
+
+    return sequences
+
+seqs = sequenceUsers(users, 20)
+
+
+
+
+
+
 
 
 # Close the database connection
