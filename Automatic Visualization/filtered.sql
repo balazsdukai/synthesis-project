39f486c51796bf1513ea70bfb9ec060599bca353
Martijn <martijnvermeer92@gmail.com>
2016-05-13 11:45:03 +0200

    changing maploc to apname for building

--- Automatic Visualization/filtered.sql
+++ Automatic Visualization/filtered.sql
@@ -2,14 +2,12 @@ drop table if exists filtered;
 
 select 
 	mac,
-	maploc,
 	asstime as start_time,
 	asstime + sesdur as end_time,
-	apname 
-into filtered
+	apname
 from wifilog
-where ((asstime+sesdur-time'03:00')::date in ({}) or (asstime-time'05:00')::date in ({}))
-and (substring(maploc,17,1) between '0' and '9' or  substring(maploc,17,1) = 'V')
+where ((asstime+sesdur-time'03:00')::date in ('2016-05-03') or (asstime-time'05:00')::date in ('2016-05-03'))
+and apname LIKE '%-%'
 order by mac,asstime;
 
 CREATE INDEX i_mac ON filtered (mac);
