ab24603092868c54423637ecaac2d71afbdea93c
balazsdukai <balazs.dukai@gmail.com>
2016-05-13 10:38:41 +0200

    testing createBs_categorized

--- buildingset_categorized.py
+++ buildingset_categorized.py
@@ -12,10 +12,10 @@ def main():
     conn, cur = uf.connectDB()
 
     buildings = bs.createBuildingsetTable(conn, cur, buildingsTable="buildings", field="buildingid",\
-                                          name="buildingset_v0511_test", mac=True)
+                                          name="buildingset_v0513_test", mac=True)
 
     createBs_categorized(conn, cur, sequenceTable='group_rec_test', id_field='mac', starttime='ts', endtime='te',\
-                         building_field='building', buildingsetTable='buildingset_v0504_test',\
+                         building_field='building', buildingsetTable='buildingset_v0513_test',\
                          building_list=buildings, mac=True, limit=5000)
 
     # Close the database connection
@@ -46,7 +46,6 @@ def createBs_categorized(conn, cur, sequenceTable, id_field, starttime, endtime,
     """
     # define time categories
     occasional = dt.timedelta(minutes=30)
-    regular = dt.timedelta(hours=5)
     often = dt.timedelta(hours=5)
 
     # Testing for potential id_field name confusions
@@ -81,6 +80,15 @@ def createBs_categorized(conn, cur, sequenceTable, id_field, starttime, endtime,
     cur.execute('select distinct '+id_field+' from '+sequenceTable+';')
     ids = cur.fetchall()
 
+    # Parse fix part of the insert query
+    fieldnames = identifier
+    for i in building_list:
+        fieldnames += ',' + i
+
+    values = '%(' + identifier + ')s'
+    for i in building_list:
+        values += ', ' + '%(' + i + ')s'
+
     # Get the visited buildings per id
     for id in ids[:limit]:
         id = id[0]
@@ -92,24 +100,23 @@ def createBs_categorized(conn, cur, sequenceTable, id_field, starttime, endtime,
                     +building_field+', nr_week) As B GROUP BY '+building_field+';')
 
         b = cur.fetchall()
-        buildingid = [(uf.getBuildingName(i[0]) for i in b] #TODO: add switch for getBuildingName in case the buildingnames are already converted
-        duration = [i[1] for i in b]
-        week = [i[2] for i in b]
-
-        # Parse insert query
-        fieldnames = identifier
-        for i in building_list:
-            fieldnames += ',' + i
-
-        values = '%('+identifier+')s'
-        for i in building_list:
-            values += ', ' + '%('+i+')s'
+        buildingname = [uf.getBuildingName(i[0]) for i in b] #TODO: add switch for getBuildingName in case the buildingnames are already converted
+        buildingset = [(uf.getBuildingName(i[0]),i[1]) for i in b]
 
         value_dict = {}
         value_dict[identifier] = id
         for i in building_list:
-            if i in b:
-                value_dict[i] = 1
+            if i in buildingname:
+                for k in buildingset:
+                    if k[0] == i:
+                        if k[1] <= occasional: # occasionally at the building
+                            value_dict[i] = 1
+                        elif occasional < k[1] <= often: # regularly at the building
+                            value_dict[i] = 2
+                        elif often < k[1]: # often at the building
+                            value_dict[i] = 3
+                    else:
+                        pass
             else:
                 value_dict[i] = None
 
@@ -124,15 +131,3 @@ if __name__ == '__main__':
     main()
 
 print("%f seconds" % (time.time() - start_time))
-
-sequenceTable = 'group_rec_test'
-id_field = 'mac'
-starttime = 'ts'
-endtime = 'te'
-building_field = 'building'
-buildingsetTable = 'buildingset_v0511_test'
-building_list = buildings
-id = 'eX0yOF0yOvt+j3RLX6L+d7S2IGlEwALpum0GuQ5gw88='
-
-print('SELECT '+building_field+', '+endtime+'-'+starttime+' as duration, EXTRACT(WEEK FROM '+starttime+\
-      ')::smallint as nr_week FROM '+sequenceTable+' where '+id_field+'=\''+id+'\';')
