f98723448cb3a35814a1345a7ceb2089a4dfda29
Matthie456 <thijssie2008@gmail.com>
2016-05-23 12:32:21 +0200

    Branches .... Branches everywhere!

--- Sequence Patterns/table_usernames.sql
+++
@@ -1,5 +0,0 @@
---create a table of the unique usernames or keep the old if already exists
-CREATE TABLE IF NOT EXISTS usernames (username text PRIMARY KEY);
-INSERT INTO usernames SELECT DISTINCT username FROM wifilog;
-CREATE INDEX i_usernames_username ON usernames USING btree (username);
-
