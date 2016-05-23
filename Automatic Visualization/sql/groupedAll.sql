2c8ef680372ccc2545e11989fd8c79760d8dd424
Matthie456 <thijssie2008@gmail.com>
2016-05-23 12:28:04 +0200

    Removing everything except report

--- Automatic Visualization/sql/groupedAll.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists groupedAll;
-
-create table groupedAll(mac text NOT NULL,building integer,
-	ts timestamp without time zone NOT NULL,te timestamp without time zone NOT NULL,
-    ap_start text, ap_end text,
-    PRIMARY KEY(mac, ts));
