e802683817c11b0e4b4e725c391251c383c470ab
balazsdukai <balazs.dukai@gmail.com>
2016-05-30 12:43:42 +0200

    testing graph builder

--- bk_network.py
+++ bk_network.py
@@ -1,11 +1,31 @@
 """
 Reference: http://docs.qgis.org/testing/en/docs/pyqgis_developer_cookbook/network_analysis.html
 """
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from qgis.core import *
 from qgis.networkanalysis import *
+from qgis.gui import *
+import shapefile
+
+paths_conn = "/var/nobackup/Google_Drive/TU-Delft_GEO1101_Group-2/data/bk_map/bk_bg_paths.shp"
+b_parts_conn = "/var/nobackup/Google_Drive/TU-Delft_GEO1101_Group-2/data/bk_map/bk_bg_centroid.shp"
+
+paths = shapefile.Reader(paths_conn)
+b_parts = shapefile.Reader(b_parts_conn)
+paths_geom = paths.shapes()
+b_parts_geom = b_parts.shapes()
+
+for i in range(len(b_parts_geom)):
+    print b_parts_geom[i].points
+    print b_parts.record(i)
+    print b_parts_geom[i].shapeType
 
 # paths = 
 # b_parts =
+
+paths = QgsVectorLayer(paths_conn, 'bk_bg_paths', "ogr")
 
 # don't use information about road direction from layer attributes,
 # all roads are treated as two-way
@@ -22,7 +42,7 @@ crs = QgsCoordinateReferenceSystem(3857, QgsCoordinateReferenceSystem.EpsgCrsId)
 builder = QgsGraphBuilder(crs)
 
 # set the start points
-pStart = points of the buildingparts come here
+pStart = QgsPoint(b_parts_geom[0].points[0][0], b_parts_geom[0].points[0][1])
 tiedPoint = director.makeGraph(builder, [pStart])
 pStart = tiedPoint[0]
 
@@ -32,6 +52,8 @@ graph = builder.graph()
 idStart = graph.findVertex(pStart)
 
 (tree, costs) = QgsGraphAnalyzer.dijkstra(graph, idStart, 0)
+
+tree = QgsGraphAnalyzer.shortestTree(graph, idStart, 0)
 
 #===============================================================================
 # Algorithm for shortest route network
