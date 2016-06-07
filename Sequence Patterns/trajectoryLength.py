dd3dd52482bf196bb8390e3ed81eb05cb02eb39a
SimonGriffioen <simon.griffioen@gmail.com>
2016-06-07 15:51:22 +0200

    simon is weer up to date

--- Sequence Patterns/trajectoryLength.py
+++ Sequence Patterns/trajectoryLength.py
@@ -1,13 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def mine(inputfile):
-    lenght = []
+    length = []
     f = open(inputfile)
     lines = f.readlines()
     f.close()
-    for line in lines[:10]:
+    for line in lines:
         traj = line.split(' ')
-        print traj, len(traj)-1
-        trajLen = len(traj)
-        lenght.append(trajLen)
+        trajLen = len(traj)-1
+        if trajLen < 20:
+            length.append(trajLen)
+    print np.mean(length)
+
+    histogram(length)
+
+
+def histogram(data):
+    bins = [2,3,4,5,6,7,8]
+    # the histogram of the data with histtype='step'
+    n, bins, patches = plt.hist(data, bins, histtype='bar', rwidth=1.0)
+    
+    #plt.hist(data)
+    plt.title("Lenght of trajectory")
+    plt.xlabel("Lenght")
+    plt.ylabel("Frequency")
+    #plt.axis([2, 10, 0, 1000000])
+    
+    plt.show()
+
 
 def main():
     mine('trajectories.txt')    
