a7dafaa02ef3362ace096a161f339e539a64d519
balazsdukai <balazs.dukai@gmail.com>
2016-06-08 15:11:45 +0200

    create traffic tables

--- Scripts/indoor_visuals_setup.sql
+++ Scripts/indoor_visuals_setup.sql
@@ -23,7 +23,7 @@ FROM (SELECT p.id, b.b_part, p.the_geom
         WHERE st_within(p.the_geom, b.geom)) AS b
 WHERE a.id = b.id;
 
--- CREATE TABLE TO STORE traffic volume
+-- CREATE TABLES TO STORE traffic volume
 CREATE TABLE if NOT EXISTS  visualization.bk_traffic(
     edge_id int4,
     cnt int4 DEFAULT 0);
@@ -33,4 +33,52 @@ INSERT INTO visualization.bk_traffic (edge_id, cnt)
 SELECT gid AS edge_id, 0
 FROM visualization.bk_paths;
 
+-- bouwpub
+CREATE TABLE if NOT EXISTS  visualization.bk_traf_bouwpub(
+    edge_id int4,
+    cnt int4 DEFAULT 0);
 
+-- fill IN the edge-ids
+INSERT INTO visualization.bk_traf_bouwpub (edge_id, cnt)
+SELECT gid AS edge_id, 0
+FROM visualization.bk_paths;
+
+--mobile
+CREATE TABLE if NOT EXISTS  visualization.bk_traf_mobile(
+    edge_id int4,
+    cnt int4 DEFAULT 0);
+
+-- fill IN the edge-ids
+INSERT INTO visualization.bk_traf_mobile (edge_id, cnt)
+SELECT gid AS edge_id, 0
+FROM visualization.bk_paths;
+
+--static
+CREATE TABLE if NOT EXISTS  visualization.bk_traf_static(
+    edge_id int4,
+    cnt int4 DEFAULT 0);
+
+-- fill IN the edge-ids
+INSERT INTO visualization.bk_traf_static (edge_id, cnt)
+SELECT gid AS edge_id, 0
+FROM visualization.bk_paths;
+
+--weekdays
+CREATE TABLE if NOT EXISTS  visualization.bk_traf_weekday(
+    edge_id int4,
+    cnt int4 DEFAULT 0);
+
+-- fill IN the edge-ids
+INSERT INTO visualization.bk_traf_weekday (edge_id, cnt)
+SELECT gid AS edge_id, 0
+FROM visualization.bk_paths;
+
+--weekends
+CREATE TABLE if NOT EXISTS  visualization.bk_traf_weekend(
+    edge_id int4,
+    cnt int4 DEFAULT 0);
+
+-- fill IN the edge-ids
+INSERT INTO visualization.bk_traf_weekend (edge_id, cnt)
+SELECT gid AS edge_id, 0
+FROM visualization.bk_paths;
