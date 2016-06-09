3dae9d5c21a38e30be5e9fc7606af77afb1d6a61
balazsdukai <balazs.dukai@gmail.com>
2016-06-09 12:11:26 +0200

    update

--- push_vis_data.sh
+++ push_vis_data.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-pg_dump -t visualization.bk_paths -t visualization.bk_paths_vertices_pgr -t visualization.bk_traffic -t visualization.bk_parts -C -h localhost -U bdukai synth_project | psql -h wifitracking.bk.tudelft.nl -U team2 wifi
+#pg_dump -t visualization.bk_paths -t visualization.bk_paths_vertices_pgr -t visualization.bk_traffic -t visualization.bk_parts -C -h localhost -U bdukai synth_project | psql -h wifitracking.bk.tudelft.nl -U team2 wifi
+
+pg_dump -t visualization.bk_traf_bouwpub -t visualization.bk_traf_canteen -C -h localhost -U bdukai synth_project | psql -h wifitracking.bk.tudelft.nl -U team2 wifi
